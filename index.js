dd7775adef021563254f02d16c6f705ae8cc1a6f
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-05-26 18:06:31 +0700

    Re-enabled CORS to test deployment

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 const errorHandling = require('./middlewares/errorHandling.js');
 const router = require('./routes');
 
-// const corsOptions = {
-//     origin: 'https://challenge1-387006.web.app', // replace with your app's URL
-//     optionsSuccessStatus: 200 
-// }
+const corsOptions = {
+    origin: 'https://challenge1-387006.web.app', // replace with your app's URL
+    optionsSuccessStatus: 200 
+}
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
