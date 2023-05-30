c8d7dba9ba6ee18d0b5f3123f1ba44fbaa8ff2d4
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-05-30 14:01:00 +0700

    Added Customers table

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const cors = require('cors');
 const errorHandling = require('./middlewares/errorHandling.js');
 const router = require('./routes');
 
-const corsOptions = {
-    origin: 'https://challenge1-387006.web.app', // replace with your app's URL
-    optionsSuccessStatus: 200 
-}
+/** SWAP CORS FOR DEV AND PROD. */
+// const corsOptions = {
+//     origin: 'https://challenge1-387006.web.app', // replace with your app's URL
+//     optionsSuccessStatus: 200 
+// }
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
