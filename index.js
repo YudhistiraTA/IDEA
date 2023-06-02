bea4c23ab02acd16cba150bf2135748671de2882
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-06-02 14:37:19 +0700

    Cors testing

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const errorHandling = require('./middlewares/errorHandling.js');
 const router = require('./routes');
 
 /** SWAP CORS FOR DEV AND PROD. */
-const corsOptions = {
-    origin: 'https://challenge1-387006.web.app', // replace with your app's URL
-    optionsSuccessStatus: 200 
-}
+// const corsOptions = {
+//     origin: 'https://challenge1-387006.web.app', // replace with your app's URL
+//     optionsSuccessStatus: 200 
+// }
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
